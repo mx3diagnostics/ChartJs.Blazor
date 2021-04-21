0041c48e4ffb58b3440dcbc640f16f96c89fc38d
John Li <jli@mx3diagnostics.com>
2021-04-21 15:55:31 +1000

    Change ScatterConfig to use LineOptions

--- src/ChartJs.Blazor/ChartJS/ScatterChart/ScatterConfig.cs
+++ src/ChartJs.Blazor/ChartJS/ScatterChart/ScatterConfig.cs
@@ -1,9 +1,10 @@
-﻿using ChartJs.Blazor.ChartJS.Common;
+using ChartJs.Blazor.ChartJS.Common;
 using ChartJs.Blazor.ChartJS.Common.Enums;
+using ChartJs.Blazor.ChartJS.LineChart;
 
 namespace ChartJs.Blazor.ChartJS.ScatterChart
 {
-    public class ScatterConfig : ConfigBase<ScatterOptions, ScatterData>
+    public class ScatterConfig : ConfigBase<LineOptions, ScatterData>
     {
         public ScatterConfig() : base(ChartType.Scatter) { }
     }
